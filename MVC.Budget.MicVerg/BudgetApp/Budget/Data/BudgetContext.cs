c059ccb8f84a9434114d0e2257d373b088de9d2b
Michiel Vergauwen <vergauwenmichiel@gmail.com>
2024-02-26 17:19:01 +0100

    added controllers

--- MVC.Budget.MicVerg/BudgetApp/Budget/Data/BudgetContext.cs
+++ MVC.Budget.MicVerg/BudgetApp/Budget/Data/BudgetContext.cs
@@ -11,12 +11,12 @@ namespace Budget.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Category>().HasData(SeedData.Categories);
-            modelBuilder.Entity<Transaction>().HasData(SeedData.Transactions);
-        }
+        //    modelBuilder.Entity<Category>().HasData(SeedData.Categories);
+        //    modelBuilder.Entity<Transaction>().HasData(SeedData.Transactions);
+        //}
     }
 }
